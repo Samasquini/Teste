d5b7f96ced641cd1f3b5f14be90e9054ee02e6e6
Samasquini <msamasquini321@gmail.com>
2023-05-30 15:27:28 -0300

    Add try catch

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+    Console.WriteLine("Digite um numero:");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Digite outro numero:");
+    int num2 = int.Parse(Console.ReadLine());
+    int result = num1 / num2;
+    Console.WriteLine("O resultado é:"+result);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Algo de errado não está certo!");
+}
